194ab64022299125e7470a9c807ae8c85e3ad74f
Douglas Johnson <douglas.johnson4@gmail.com>
2024-05-28 14:01:48 +0100

    Use graphql API

--- src/service/DeckService.ts
+++ src/service/DeckService.ts
@@ -2,29 +2,93 @@ import Http from './Http';
 import PersistedDeck from '../types/PersistedDeck';
 import Deck from '../types/Deck';
 
+const DECK_FIELDS = `{
+      id
+      name
+      imageUrl
+      attributes {
+        name
+        units
+      }
+      cards {
+        name
+        description
+        imageUrl
+        attributes {
+          type
+          value
+        }
+      }
+    }`;
+const DECKS_QUERY = `
+query {
+  decks ${DECK_FIELDS}
+}`;
+
 const findAll = () =>
   new Promise<PersistedDeck[]>((resolve, reject) =>
-    Http.get<PersistedDeck[]>('/decks')
-      .then((response) => resolve(response.data))
+    Http.post('/decks', {
+      query: DECKS_QUERY,
+    })
+      .then((response) => resolve(response.data.data.decks))
       .catch(reject),
   );
-const save = (deck: Deck) =>
-  new Promise<PersistedDeck>((resolve, reject) =>
-    Http.post<PersistedDeck>('/decks', deck)
-      .then((response) => resolve(response.data))
+const SAVE_MUTATION = `
+mutation ($deck: CreateOnedecksInput!) {
+  deckCreate(record: $deck) {
+    record ${DECK_FIELDS}
+  }
+}`;
+const save = (deck: Deck) => {
+  return new Promise<PersistedDeck>((resolve, reject) =>
+    Http.post('/decks', {
+      query: SAVE_MUTATION,
+      variables: {
+        deck,
+      },
+    })
+      .then((response) => resolve(response.data.data.deckCreate.record))
       .catch(reject),
   );
-const update = (deck: PersistedDeck) =>
-  new Promise<PersistedDeck>((resolve, reject) =>
-    Http.patch<PersistedDeck>(`/decks/${deck.id}`, deck)
-      .then((response) => resolve(response.data))
+};
+const UPDATE_MUTATION = `
+mutation ($id: MongoID!, $deck: UpdateByIddecksInput!) {
+  deckUpdate(_id: $id, record: $deck) {
+    record ${DECK_FIELDS}
+  }
+}`;
+const update = (deck: PersistedDeck) => {
+  return new Promise<PersistedDeck>((resolve, reject) =>
+    Http.post('/decks', {
+      query: UPDATE_MUTATION,
+      variables: {
+        id: deck.id,
+        deck: { ...deck, id: undefined },
+      },
+    })
+      .then((response) => resolve(response.data.data.deckUpdate.record))
       .catch(reject),
   );
-const deleteDeck = (deck: PersistedDeck) =>
-  new Promise<PersistedDeck>((resolve, reject) =>
-    Http.delete(`/decks/${deck.id}`)
+};
+const DELETE_MUTATION = `
+mutation ($id: MongoID!) {
+  deckDelete(_id: $id) {
+    record {
+      id
+    }
+  }
+}`;
+const deleteDeck = (deck: PersistedDeck) => {
+  return new Promise<PersistedDeck>((resolve, reject) =>
+    Http.post('/decks', {
+      query: DELETE_MUTATION,
+      variables: {
+        id: deck.id,
+      },
+    })
       .then(() => resolve(deck))
       .catch(reject),
   );
+};
 
 export { findAll, save, update, deleteDeck };
