411f82f072f4d045ff543cdb5e1635f9008a4514
Douglas Johnson <douglas.johnson4@gmail.com>
2024-06-03 22:02:34 +0100

    authentication (#8)

--- src/service/Http.ts
+++ src/service/Http.ts
@@ -2,4 +2,15 @@ import { default as axios } from 'axios';
 
 axios.defaults.baseURL = '/api';
 
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
 export default axios;
